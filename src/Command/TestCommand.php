be357e31be384dea08e9c082560f9ffb8961c607
nicholaskevs <nekoman.hunter.tech@gmail.com>
2023-02-15 01:53:02 +1100

    Create test command

---
+++ src/Command/TestCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Request;
+
+class TestCommand extends UserCommand
+{
+	protected $name = 'test';
+	protected $description = 'A command for test';
+	protected $usage = '/test';
+	protected $version = '1.0.0';
+	
+	public function execute(): ServerResponse
+	{
+		$message = $this->getMessage();
+		
+		$chat_id = $message->getChat()->getId();
+		
+		$data = [
+			'chat_id'	=> $chat_id,
+			'text'		=> 'This is just a Test...',
+		];
+		
+		// return Request::sendMessage($data);
+		return $this->replyToChat(
+			'Hi there!' . PHP_EOL .
+			'This is just a Test!'
+		);
+	}
+}
--- src/main.php
+++ src/main.php
@@ -3,5 +3,41 @@
 require dirname(__DIR__).'/vendor/autoload.php';
 require dirname(__DIR__).'/config/cons.php';
 
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Exception\TelegramLogException;
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\TelegramLog;
 use TelegramBot\Lib\Bot;
 
+try {
+	$bot = new Telegram(BOT_TOKEN, BOT_USERNAME);
+	
+	$bot->enableMySql([
+		'host'		=> DB_HOST,
+		'user'		=> DB_USERNAME,
+		'password'	=> DB_PASSWORD,
+		'database'	=> DB_DBNAME,
+	]);
+	
+	$bot->addCommandsPath(dirname(__DIR__).'/src/Command');
+	
+	$server_response = $bot->handleGetUpdates();
+	
+	if ($server_response->isOk()) {
+		$update_count = count($server_response->getResult());
+		echo date('Y-m-d H:i:s') . ' - Processed ' . $update_count . ' updates' . PHP_EOL;
+	} else {
+		echo date('Y-m-d H:i:s') . ' - Failed to fetch updates' . PHP_EOL;
+		echo $server_response->printError();
+	}
+	
+} catch(TelegramException $e) {
+	TelegramLog::error($e);
+	echo $e;
+	
+} catch(TelegramLogException $e) {
+	echo $e;
+	
+} catch(Exception $e) {
+	echo $e->getMessage();
+}
