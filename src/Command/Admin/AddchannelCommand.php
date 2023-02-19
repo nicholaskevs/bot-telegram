838c883a31df77e55010caa7d8740dc66348c85f
nicholaskevs <nekoman.hunter.tech@gmail.com>
2023-02-19 23:21:00 +1100

    Disable addchannel command

--- src/Command/Admin/AddchannelCommand.php
+++ src/Command/Admin/AddchannelCommand.php
@@ -13,6 +13,7 @@ class AddchannelCommand extends AdminCommand
 	protected $description = 'Add channel to database';
 	protected $usage = '/addchannel <channel_title> or /addchannel <channel_id>';
 	protected $version = '1.0.0';
+    protected $enabled = false;
     protected $need_mysql = true;
 
 	public function execute(): ServerResponse
