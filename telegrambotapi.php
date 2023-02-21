335112034899803580687ad5f38637aa50bf4e28
nicholaskevs <nekoman.hunter.tech@gmail.com>
2023-02-21 16:42:34 +1100

    Add plan for later

---
+++ telegrambotapi.php
@@ -0,0 +1,5 @@
+<?php
+
+// process api key and command here
+// remove forwarder? discord call this api to forward message, add telegram_id on otherbot.forwarder
+// centralise send to channel command? process all in telegrambot.php or make cli folder? remove cli support?
